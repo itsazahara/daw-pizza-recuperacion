bef3d01a89c79de47a008a5a916b3ad5f08b0256
itsazahara <aromsch1403@g.educaand.es>
2025-03-17 12:30:28 +0100

    Creacion de nuevos mapper y modificación de los mapper ya existentes

--- src/main/java/com/daw/services/mappers/PedidoMapper.java
+++ src/main/java/com/daw/services/mappers/PedidoMapper.java
@@ -9,31 +9,27 @@ import com.daw.services.dtos.PedidoDTO;
 import com.daw.services.dtos.PizzaPedidoOutputDTO;
 
 public class PedidoMapper {
-	
+
 	public static PedidoDTO toDto(Pedido pedido) {
 		PedidoDTO dto = new PedidoDTO();
-		
-		//Vamos recogiendo los atributos de PedidoDTO de las distintas entidades
-		//Vienen de Pedido
+
 		dto.setId(pedido.getId());
 		dto.setFecha(pedido.getFecha());
 		dto.setTotal(pedido.getTotal());
 		dto.setMetodo(pedido.getMetodo());
 		dto.setNotas(pedido.getNotas());
-		
-		//Vienen del Cliente asociado al Pedido
+
 		dto.setCliente(pedido.getCliente().getNombre());
 		dto.setTelefono(pedido.getCliente().getTelefono());
-		dto.setDireccion(pedido.getCliente().getDireccion());
-		
+
 		List<PizzaPedidoOutputDTO> pizzas = new ArrayList<PizzaPedidoOutputDTO>();
-		
-		for(PizzaPedido pp : pedido.getPizzaPedidos()) {
+
+		for (PizzaPedido pp : pedido.getPizzaPedidos()) {
 			pizzas.add(PizzaPedidoMapper.toDTO(pp));
 		}
-		
+
 		dto.setPizzas(pizzas);
-		
+
 		return dto;
 	}
 
--- src/main/java/com/daw/services/mappers/PizzaPedidoMapper.java
+++ src/main/java/com/daw/services/mappers/PizzaPedidoMapper.java
@@ -5,27 +5,27 @@ import com.daw.services.dtos.PizzaPedidoInputDTO;
 import com.daw.services.dtos.PizzaPedidoOutputDTO;
 
 public class PizzaPedidoMapper {
-	
+
 	public static PizzaPedidoOutputDTO toDTO(PizzaPedido pizzaPedido) {
 		PizzaPedidoOutputDTO dto = new PizzaPedidoOutputDTO();
-		
+
 		dto.setId(pizzaPedido.getId());
 		dto.setIdPizza(pizzaPedido.getIdPizza());
 		dto.setCantidad(pizzaPedido.getCantidad());
 		dto.setPrecio(pizzaPedido.getPrecio());
-		dto.setPizza(pizzaPedido.getPizza().getNombre());		
-		
+		dto.setPizza(pizzaPedido.getPizza().getNombre());
+
 		return dto;
 	}
-	
+
 	public static PizzaPedido toEntity(PizzaPedidoInputDTO dto) {
 		PizzaPedido pp = new PizzaPedido();
-		
+
 		pp.setId(dto.getId());
 		pp.setIdPizza(dto.getIdPizza());
 		pp.setIdPedido(dto.getIdPedido());
 		pp.setCantidad(dto.getCantidad());
-		
+
 		return pp;
 	}
 
